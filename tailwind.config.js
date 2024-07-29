3a8b425e24269f08118b6a6047fc9ab9511384d3
Alex Mercy Adeiza <alexmercyadeiza@gmail.com>
2024-07-29 21:51:34 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["Montserrat"],
+      },
+      colors: {
+        dorange: "#BF6742",
+        lorange: "#ECE7E1",
+        morange: "#BFAE99",
       },
     },
   },
